c4447990e7ee1b6430b75e908e2788973c0327fd
hatkid <daichi93100@gmail.com>
2021-09-22 16:56:46 +0200

    better discord embed

--- invite.js
+++ invite.js
@@ -1,7 +1,7 @@
 //I use that for invites, do whatever you want with it but its not my problem if something goes wrong
-
-const DISCORD_URL = "https://discord.com/api/invites/";
-const DISCORD_ICONS = "https://cdn.discordapp.com/icons/"
+//also mention me for the code
+const DISCORD_URL = "https://discord.com/api/v9/invites/";
+const DISCORD_ICONS = "https://cdn.discordapp.com/icons/";
 function httpGetAsync(theUrl, callback) {
     let xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = () => {
@@ -12,12 +12,21 @@ function httpGetAsync(theUrl, callback) {
     xmlHttp.send(null);
 }
 function addDiscordInviteEmbed(code) {
-	httpGetAsync(`${DISCORD_URL}${code}?with_counts=true`, (response) => {
-		let obj = JSON.parse(response)
-		let name = obj.guild.name
-		let url = `${DISCORD_ICONS}${obj.guild.id}/${obj.guild.icon}.png`
+	fetch(`${DISCORD_URL}${code}?with_counts=true`).then((response) => {
+		try {
+            response.text().then((r) => {
+                let obj = JSON.parse(r)
+                let name = obj.guild.name;
+    		    let url = `${DISCORD_ICONS}${obj.guild.id}/${obj.guild.icon}.png`;
 
-		let embed = document.getElementById(`discord-${code}`)
-		embed.innerHTML = `<strong>You are invited to join a server</strong><br><strong><img src="${url}" style=" margin-left:2%;float:left;border-radius:15% " width="64" height="64"/><a href="https://discord.com/${code}">${name}</a><br>${obj.approximate_presence_count} online  ${obj.approximate_member_count} members`
+        		let embed = document.getElementById(`discord-${code}`);
+        		embed.innerHTML = `
+                <strong>You are invited to join a server</strong><br>
+                <strong><img src="${url}" style=" margin-left:2%;float:left;border-radius:15% " width="64" height="64"/><a href="https://discord.com/invite/${code}">${name}</a><br>
+                <span class="online"></span>${obj.approximate_presence_count} online <span class="members"></span>${obj.approximate_member_count} members`;
+            })
+        } catch(e) {
+            console.error(e)
+        }
 	});
 }
