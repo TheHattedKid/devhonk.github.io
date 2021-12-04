11026d3e7513105d9f50de9524ddf9f2e5a98a54
hatkid <daichi93100@gmail.com>
2021-12-04 23:25:25 +0100

    a

---
+++ day4.js
@@ -0,0 +1,126 @@
+const { cp } = require("fs");
+const { exit } = require("process");
+
+const arrayWithoutElementAtIndex = (arr, index) => arr.filter((value, arrIndex) => index !== arrIndex);
+let input = require("fs").readFileSync("./in.txt").toString().split('\n');
+let nums = eval('[' + input[0] + ']');  // cringe.
+let input1 = input.splice(2);
+let boards = [];
+let currBoard = [];
+let currBoardI = 0;
+for (let line of input1) {
+  line = line.trimLeft().split(/ +/gm);
+  if (line[0] === '') {
+    boards.push(currBoard.filter(n => n));
+    currBoard = [];
+    continue;
+  }
+  currBoard.push(line);
+}
+boards.push(currBoard.filter(n => n));
+currBoard = [];
+boards = arrayWithoutElementAtIndex(boards, boards.length - 1)
+
+function markWithNumber(n) {
+  for (let i = 0; i < boards.length; i++) {
+    let board = boards[i];
+    for (let j = 0; j < board.length; j++) {
+      let arr = board[j];
+      for (let k = 0; k < arr.length; k++) {
+        let n1 = parseInt(arr[k], 10);
+        if (n1 === n) {
+          boards[i][j][k] = `*${boards[i][j][k]}`;
+        }
+      }
+    }
+  }
+}
+function unMarkWithNumber(n) {
+  for (let i = 0; i < boards.length; i++) {
+    let board = boards[i];
+    for (let j = 0; j < board.length; j++) {
+      let arr = board[j];
+      for (let k = 0; k < arr.length; k++) {
+        let n1 = parseInt(arr[k].replace("*", ""), 10);
+        if (n1 === n) {
+          boards[i][j][k] = `${boards[i][j][k].replace("*", "")}`;
+        }
+      }
+    }
+  }
+}
+function detectWinner() {
+  // Detect horizontally
+  for (let i = 0; i < boards.length; i++) {
+    let board = boards[i];
+    for (let j = 0; j < board.length; j++) {
+      let arr = board[j];
+      if (arr.filter((v) => v.startsWith('*')).length === arr.length) {
+        return i;
+      }
+    }
+  }
+  // Detect vertically
+  for (let i = 0; i < boards.length; i++) {
+    let board = boards[i];
+    for (let j = 0; j < board.length; j++) {
+      let arr = [];
+      for (let k = 0; k < board.length; k++) {
+        arr.push(board[k][j]);
+      }
+      if (arr.filter((v) => v.startsWith('*')).length === arr.length) {
+        return i;
+      }
+    }
+  }
+  return undefined;
+}
+let winners = [];
+function calculate(winner, number) {
+  //let winningBoard = boards[winner];
+  let out = 0;
+  for (let line of winner) {
+    for (let str of line) {
+      if (!str.startsWith('*')) {
+        out += parseInt(str, 10);
+      }
+    }
+  }
+  console.log(out, "*", number);
+  return out * number;
+}
+let a = false;
+let b1 = boards;
+let lwinner = undefined;
+let llwinner = null;
+//console.log(boards)
+let preWinner;
+let num = 0;
+let ab = false;
+for(let i = 0; i < nums.length; i++) {
+  if(i === nums.length - 1 && !ab) {
+    winners.push(boards[0]);
+    break;
+  }
+  num = nums[i];
+  console.log("Drawing", num, "!");
+  markWithNumber(num);
+  if(ab) {
+    console.log("e")
+    ab = false;
+    winners.push(boards[0]);
+    break;
+  }
+  let winner = detectWinner();
+  
+  if(winner !== undefined) {
+
+    boards = arrayWithoutElementAtIndex(boards, winner);
+    if(boards.length === 1) {
+      ab = true;
+    }
+  }
+}
+let last = winners[0];
+console.log(last);
+console.log(calculate(last, num));
